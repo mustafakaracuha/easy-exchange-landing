0570788ba276eb8434d54a8ffd211b740557fc02
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-10-05 23:00:07 +0300

    login hover edit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,7 +10,7 @@ const Header = ({ onLoginClick }: HeaderProps) => {
       <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-8">
           <img src={Logo} alt="logo" className="w-52 h-16 md:w-14 md:h-14 lg:w-72 lg:h-10" />
         <nav className="flex items-center gap-6">
-          <button onClick={onLoginClick} className="text-slate-600 hover:text-slate-900 transition-colors">LOGIN</button>
+          <button onClick={onLoginClick} className="text-slate-600 cursor-pointer hover:text-lime-600 transition-colors">LOGIN</button>
         </nav>
       </div>
     </header>
